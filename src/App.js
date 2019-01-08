539a6dac77fe5641bec8106b48ed5d30d48db251
Joseph Lee <jin3lee@gmail.com>
2019-01-08 01:43:19 -0800

    write scripts to solve test problem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const string = 'You will deliver new technology with an adorable puppy. Perfect!';
+
 const highlights = [
   {
       startOffset: 4,
@@ -13,10 +14,57 @@ const highlights = [
   {
       startOffset: 17,
       endOffset: 31,
-      color: '#e8e8e8',
+      color: '#000000',
       priority: 1,
+  },
+  {
+      startOffset: 25,
+      endOffset: 50,
+      color: '#e8e8e8',
+      priority: 5,
+  },
+  {
+      startOffset: 55,
+      endOffset: 59,
+      color: '#FFFFFF',
+      priority: 2,
   }
 ];
+
+// sort highlight array by priority
+highlights.sort( _highlightCompareFunction );
+
+// initialize color array with null
+var indexColorArray = [];
+for( var i = 0; i < string.length; i++ ) {
+  indexColorArray.push(null);
+}
+
+// fill boolean array with background color for each character in 'string'
+for( var j = 0; j < highlights.length; j++ ) {
+  for( var i = 0; i < string.length; i++ ) {
+    if( _charIndexIsWithinHighlightObjectRange( highlights[j] , i ) ) {
+      indexColorArray[i] = highlights[j].color;
+    }
+  }
+}
+
+// returns true if index is between the highlight param's startOffset & endOffset
+function _charIndexIsWithinHighlightObjectRange ( highlight, index ) {
+  return highlight.startOffset <= index && index <= highlight.endOffset;
+}
+
+// comparing highlight objects function
+function _highlightCompareFunction ( a, b ) {
+  if( a.priority < b.priority ) {
+      return -1;
+  } else if( a.priority > b.priority ) {
+      return 1;
+  } else {
+      return 0;
+  }
+}
+
 
 class App extends Component {
   render() {
